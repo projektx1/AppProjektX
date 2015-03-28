c1020d242af0a6f27927c346da4ff74b6327d1d8
projektx1 <myprojektx1@gmail.com>
2015-03-28 23:06:43 +0100

    commit

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module AppProjektX
         :domain               => 'mail.google.com',
         :port                 => 587,
         :user_name            => 'myprojektx1@gmail.com',
-        :password             =>'XXXXXXX',
+        :password             =>'XXXXXXX..',
         :authentication       => 'login',
         :enable_starttls_auto => true
     }
